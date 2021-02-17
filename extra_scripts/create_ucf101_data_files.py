293c9cd1d398cd07db890bc0dd4113efc85256e1
QuentinDuval <QuentinDuval@users.noreply.github.com>
2021-02-17 10:31:35 -0500

    Align the UCF-101 dataset with the remaining data preparation scripts

--- extra_scripts/create_ucf101_data_files.py
+++ extra_scripts/create_ucf101_data_files.py
@@ -1,10 +1,14 @@
 import argparse
 import os
+from contextlib import contextmanager
 from typing import Optional, List, Tuple
 
 from PIL import Image
 from torch.utils.data import DataLoader
+from torchvision.datasets.utils import download_url, extract_archive
 from tqdm import tqdm
+from pyunpack import Archive
+import ssl
 
 try:
     import av
@@ -12,30 +16,58 @@ except ImportError:
     raise ValueError("You must have pyav installed to run this script: pip install av.")
 
 
-def extract_mid_frame(file_path: str) -> Optional[Image.Image]:
+def get_argument_parser():
     """
-    Extract the middle frame out of a video clip.
+    List of arguments supported by the script
     """
-    with av.open(file_path) as container:
-        nb_frames = container.streams.video[0].frames
-        vid_stream = container.streams.video[0]
-        for i, frame in enumerate(container.decode(vid_stream)):
-            if i - 1 == nb_frames // 2:
-                return frame.to_image()
-        return None
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "-i",
+        "--input",
+        type=str,
+        help="The input folder contains the expanded UCF-101 archive files",
+    )
+    parser.add_argument(
+        "-o",
+        "--output",
+        type=str,
+        help="The output folder containing the disk_folder output",
+    )
+    parser.add_argument(
+        "-d",
+        "--download",
+        action="store_const",
+        const=True,
+        default=False,
+        help="To download the original dataset and decompress it in the input folder",
+    )
+    return parser
 
 
-def extract_split_info(file_path: str) -> List[Tuple[str, str]]:
+@contextmanager
+def without_ssl_certificate_check():
+    default_context = ssl._create_default_https_context
+    ssl._create_default_https_context = ssl._create_unverified_context
+    yield
+    ssl._create_default_https_context = default_context
+
+
+def download_dataset(root: str):
     """
-    Parse an annotation file of ucf101 and list the samples, grouped by category
+    Download the UCF101 dataset archive and expand it in the folder provided as parameter
     """
-    samples = []
-    with open(file_path) as f:
-        for line in f:
-            category, file_name = line.strip().split("/")
-            file_name = file_name.split(" ")[0]
-            samples.append((category, file_name))
-    return samples
+    IMAGE_URL = "https://www.crcv.ucf.edu/data/UCF101/UCF101.rar"
+    SPLIT_URL = "https://www.crcv.ucf.edu/data/UCF101/UCF101TrainTestSplits-RecognitionTask.zip"
+
+    # Download the raw inputs of UCF101, circumventing the SSL certificate issues
+    with without_ssl_certificate_check():
+        download_url(url=IMAGE_URL, root=root)
+        download_url(url=SPLIT_URL, root=root)
+
+    # Extract the archives
+    print("Extracting archives...")
+    Archive(os.path.join(root, "UCF101.rar")).extractall(root)
+    extract_archive(os.path.join(root, "UCF101TrainTestSplits-RecognitionTask.zip"))
 
 
 class _ExtractMiddleFrameDataset:
@@ -45,40 +77,52 @@ class _ExtractMiddleFrameDataset:
 
     def __init__(self, data_path: str, annotation_path: str):
         self.data_path = data_path
-        self.split_info = extract_split_info(annotation_path)
+        self.split_info = self._read_split_info(annotation_path)
+
+    @staticmethod
+    def _read_split_info(file_path: str) -> List[Tuple[str, str]]:
+        samples = []
+        with open(file_path) as f:
+            for line in f:
+                category, file_name = line.strip().split("/")
+                file_name = file_name.split(" ")[0]
+                samples.append((category, file_name))
+        return samples
+
+    @staticmethod
+    def _extract_middle_frame(file_path: str) -> Optional[Image.Image]:
+        """
+        Extract the middle frame out of a video clip.
+        """
+        with av.open(file_path) as container:
+            nb_frames = container.streams.video[0].frames
+            vid_stream = container.streams.video[0]
+            for i, frame in enumerate(container.decode(vid_stream)):
+                if i - 1 == nb_frames // 2:
+                    return frame.to_image()
+            return None
 
     def __len__(self):
         return len(self.split_info)
 
     def __getitem__(self, idx: int) -> Tuple[Image.Image, str, str]:
         category, video_name = self.split_info[idx]
-        video_path = os.path.join(self.data_path, video_name)
-        mid_frame = extract_mid_frame(video_path)
+        video_path = os.path.join(self.data_path, category, video_name)
+        mid_frame = self._extract_middle_frame(video_path)
         image_name = os.path.splitext(video_name)[0] + ".jpg"
         return mid_frame, image_name, category
 
 
-def create_disk_folder(
-        annotation_path: str,
-        data_path: str,
-        output_path: str,
-        num_workers: int,
-):
+def create_disk_folder_split(annotation_path: str, data_path: str, output_path: str):
     """
-    Read the annotation path, extract from it the split information, and create a folder with the following structure:
-
-        Action1/
-            Image1
-            ...
-        Action2/
-            Image2
-            ...
+    Create one split of the disk_folder format from the file at 'annotation_path' and the data stored
+    in the folder 'data_path'.
     """
     assert os.path.exists(annotation_path), f"Could not find annotation path {annotation_path}"
     assert os.path.exists(data_path), f"Could not find data folder {data_path}"
 
     dataset = _ExtractMiddleFrameDataset(data_path=data_path, annotation_path=annotation_path)
-    loader = DataLoader(dataset, num_workers=num_workers, batch_size=1, collate_fn=lambda x: x[0])
+    loader = DataLoader(dataset, num_workers=8, batch_size=1, collate_fn=lambda x: x[0])
     for batch in tqdm(loader):
         mid_frame, image_name, category = batch
         category_folder = os.path.join(output_path, category)
@@ -88,35 +132,27 @@ def create_disk_folder(
             mid_frame.save(image_file)
 
 
-def get_argument_parser():
-    parser = argparse.ArgumentParser()
-    parser.add_argument('-d', '--data', type=str, help="Data path")
-    parser.add_argument('-a', '--annotation', type=str, help="File annotation path")
-    parser.add_argument('-o', '--output', type=str, help="Output folder")
-    parser.add_argument('-w', '--workers', type=int, default=8, help="Number of parallel workers")
-    return parser
-
-
 if __name__ == '__main__':
     """
     Example usage:
 
     ```
-    python extra_scripts/create_ucf101_data_files.py \
-        -d /path/to/ucf101/data \
-        -a /path/to/ucf101/ucfTrainTestlist/trainlist01.txt \
-        -o /output_path/ucf101/train
-
-    python extra_scripts/create_ucf101_data_files.py \
-        -d /path/to/ucf101/data \
-        -a /path/to/ucf101/ucfTrainTestlist/testlist01.txt \
-        -o /output_path/ucf101/test
+    python extra_scripts/create_ucf101_data_files.py -i /path/to/ucf101 -o /output_path/ucf101 -d
     ```
     """
     args = get_argument_parser().parse_args()
-    create_disk_folder(
-        annotation_path=args.annotation,
-        data_path=args.data,
-        output_path=args.output,
-        num_workers=args.workers
+    if args.download:
+        download_dataset(args.input)
+
+    data_path = os.path.join(args.input, "UCF-101")
+    annotation_path = os.path.join(args.input, "ucfTrainTestlist")
+    create_disk_folder_split(
+        annotation_path=os.path.join(annotation_path, "trainlist01.txt"),
+        data_path=data_path,
+        output_path=os.path.join(args.output, "train"),
+    )
+    create_disk_folder_split(
+        annotation_path=os.path.join(annotation_path, "testlist01.txt"),
+        data_path=data_path,
+        output_path=os.path.join(args.output, "val"),
     )
