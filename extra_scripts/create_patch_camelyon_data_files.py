225e1f0ad46c577782afab185dea616c017376fe
QuentinDuval <QuentinDuval@users.noreply.github.com>
2021-02-17 09:52:35 -0500

    Align the PCAM dataset with the remaining data preparation scripts

--- extra_scripts/create_patch_camelyon_data_files.py
+++ extra_scripts/create_patch_camelyon_data_files.py
@@ -1,12 +1,11 @@
 import argparse
 import os
 from collections import OrderedDict
-from typing import NamedTuple, Tuple
+from typing import NamedTuple
 
 import h5py
 from PIL import Image
 from torch.utils.data import DataLoader
-from torchvision.datasets import VisionDataset
 from torchvision.datasets.utils import download_and_extract_archive
 from tqdm import tqdm
 
@@ -36,12 +35,6 @@ def get_argument_parser():
         default=False,
         help="To download the original dataset and decompress it in the input folder",
     )
-    parser.add_argument(
-        '-w',
-        '--workers',
-        type=int,
-        default=8,
-        help="Number of parallel workers")
     return parser
 
 
@@ -50,7 +43,7 @@ class Split(NamedTuple):
     url: str
 
 
-class PatchCamelyon(VisionDataset):
+class PatchCamelyon:
     """
     Dataset used to parallelize the transformation of the dataset via a DataLoader
     """
@@ -76,66 +69,71 @@ class PatchCamelyon(VisionDataset):
         }
     )
 
-    def __init__(
-        self, root: str, split: str, transform=None, target_transform=None, download: bool = False
-    ):
-        super().__init__(
-            root=root, transform=transform, target_transform=target_transform
-        )
-
-        # Checking the presence of the data and downloading it if not present
-        for file, url in self._FILES.values():
-            if not os.path.exists(os.path.join(root, file)):
-                if download:
-                    download_and_extract_archive(url=url, download_root=root)
-                else:
-                    raise ValueError(f"Missing file {file} in {root}")
-
-        # Loading the data
+    def __init__(self, input_path: str, output_path: str, split: str, download: bool = False):
+        self.input_path = input_path
+        self.output_path = output_path
+        self.split = split
+        self._download_missing_files(download)
         self.x = self._open_h5_file(self._FILES[f"{split}_x"].file)["x"]
         self.y = self._open_h5_file(self._FILES[f"{split}_y"].file)["y"]
 
+    def _download_missing_files(self, download: bool):
+        for file, url in self._FILES.values():
+            if not os.path.exists(os.path.join(self.input_path, file)):
+                if download:
+                    download_and_extract_archive(url=url, download_root=self.input_path)
+                else:
+                    raise ValueError(f"Missing file {file} in {self.input_path}")
+
     def _open_h5_file(self, file_name: str):
-        return h5py.File(os.path.join(self.root, file_name), "r")
+        return h5py.File(os.path.join(self.input_path, file_name), "r")
 
     def __len__(self):
         return self.x.shape[0]
 
-    def __getitem__(self, idx: int) -> Tuple[Image.Image, int]:
+    def __getitem__(self, idx: int) -> bool:
         img = Image.fromarray(self.x[idx])
         label = self.y[idx][0][0]
-        return img, label
+        folder = os.path.join(self.output_path, "tumor" if label == 1 else "no_tumor")
+        img.save(os.path.join(folder, f"img_{idx}.jpg"))
+        return True
 
 
-def to_disk_folder(dataset: PatchCamelyon, output_folder: str, num_workers: int):
+def to_disk_folder_split(dataset: PatchCamelyon, output_folder: str, num_workers: int):
+    """
+    Create one split of the disk_folder format
+    """
     os.makedirs(os.path.join(output_folder, "tumor"), exist_ok=True)
     os.makedirs(os.path.join(output_folder, "no_tumor"), exist_ok=True)
     loader = DataLoader(dataset, num_workers=num_workers, batch_size=1, collate_fn=lambda x: x[0])
-    for i, (x, y) in enumerate(tqdm(loader)):
-        folder = os.path.join(output_folder, "tumor" if y == 1 else "no_tumor")
-        with open(os.path.join(folder, f"img_{i}.jpg"), "w") as img_file:
-            x.save(img_file)
+    with tqdm(total=len(dataset)) as progress_bar:
+        for _ in loader:
+            progress_bar.update(1)
 
 
-def create_data_files():
-    # Parse command arguments
-    parser = get_argument_parser()
-    args = parser.parse_args()
-    input_folder = os.path.expanduser(args.input)
-    output_folder = os.path.expanduser(args.output)
-    output_train_folder = os.path.join(output_folder, "train")
-    output_valid_folder = os.path.join(output_folder, "val")
-    if args.download:
-        os.makedirs(input_folder, exist_ok=True)
+def create_data_files(input_path: str, output_path: str, download: bool):
+    input_path = os.path.expanduser(input_path)
+    output_path = os.path.expanduser(output_path)
+    if download:
+        os.makedirs(input_path, exist_ok=True)
 
-    # Create the dataset and optionally downloads it
-    print(args.download)
-    train_set = PatchCamelyon(root=input_folder, split="train", download=args.download)
-    valid_set = PatchCamelyon(root=input_folder, split="valid", download=False)
+    # Create the training split
+    output_train_folder = os.path.join(output_path, "train")
+    train_set = PatchCamelyon(
+        input_path=input_path,
+        output_path=output_train_folder,
+        split="train",
+        download=download)
+    to_disk_folder_split(train_set, output_train_folder, num_workers=8)
 
-    # Transform the datasets to disk_folder
-    to_disk_folder(train_set, output_train_folder, num_workers=args.workers)
-    to_disk_folder(valid_set, output_valid_folder, num_workers=args.workers)
+    # Create the validation split
+    output_valid_folder = os.path.join(output_path, "val")
+    valid_set = PatchCamelyon(
+        input_path=input_path,
+        output_path=output_valid_folder,
+        split="valid",
+        download=False)
+    to_disk_folder_split(valid_set, output_valid_folder, num_workers=8)
 
 
 if __name__ == "__main__":
@@ -143,10 +141,12 @@ if __name__ == "__main__":
     Example usage:
 
     ```
-    python extra_scripts/create_patch_camelyon_data_files.py \
-        -i /path/to/pcam \
-        -o /output_path/to/pcam \
-        -d
+    python extra_scripts/create_patch_camelyon_data_files.py -i /path/to/pcam -o /output_path/to/pcam -d
     ```
     """
-    create_data_files()
+    args = get_argument_parser().parse_args()
+    create_data_files(
+        input_path=args.input,
+        output_path=args.output,
+        download=args.download
+    )
